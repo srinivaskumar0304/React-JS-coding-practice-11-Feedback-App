8e7261af8405c523a0560fdb371592c48629db6e
Srinivas kumar <seenukumar124@gmail.com>
2022-11-04 10:26:36 +0000

    React JS coding practice 11 Feedback App

---
+++ src/components/Feedback/index.js
@@ -0,0 +1,69 @@
+// Write your React code here.
+import {Component} from 'react'
+
+import './index.css'
+
+class Feedback extends Component {
+  state = {
+    isFeedbackSelected: false,
+  }
+
+  onClickEmoji = () => this.setState({isFeedbackSelected: true})
+
+  feedbackQuestion = () => {
+    const {resources} = this.props
+    const {emojis} = resources
+
+    return (
+      <div className="feedback-question-container">
+        <h1 className="feedback-question">
+          How satisfied are you with our customer support performance?
+        </h1>
+        <ul className="emojis-list">
+          {emojis.map(emoji => (
+            <li key={emoji.id}>
+              <button
+                type="button"
+                className="emoji-button"
+                onClick={this.onClickEmoji}
+              >
+                <img src={emoji.imageUrl} alt={emoji.name} className="emoji" />
+                <br />
+                <span className="emoji-name">{emoji.name}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  thankYouScreen = () => {
+    const {resources} = this.props
+    const {loveEmojiUrl} = resources
+
+    return (
+      <div className="thank-you-container">
+        <img src={loveEmojiUrl} alt="love emoji" className="love-emoji" />
+        <h1 className="thank-you-text">Thank You!</h1>
+        <p className="description">
+          We will use your feedback to improve our customer support performance.
+        </p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isFeedbackSelected} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="feedback-card">
+          {isFeedbackSelected ? this.thankYouScreen() : this.feedbackQuestion()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Feedback
